68096248c05ebbe0b0af668c4cd60031b2fbdb1b
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-04-15 02:33:35 -0300

    Change account service to use config server

--- accountservice/go.mod
+++ accountservice/go.mod
@@ -3,7 +3,9 @@ module github.com/maxsuelmarinho/golang-microservices-example/accountservice
 require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gorilla/mux v1.7.1
+	github.com/pelletier/go-toml v1.3.0 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
+	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.3.0
 	gopkg.in/h2non/gock.v1 v1.0.14
 )
--- accountservice/main.go
+++ accountservice/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/maxsuelmarinho/golang-microservices-example/accountservice/config"
 	"github.com/maxsuelmarinho/golang-microservices-example/accountservice/dbclient"
 	"github.com/maxsuelmarinho/golang-microservices-example/accountservice/service"
+	"github.com/spf13/viper"
 )
 
 var appName = "accountservice"
 
+func init() {
+	profile := flag.String("profile", "test", "Environment profile")
+	configServerURL := flag.String("configServerUrl", "http://config-server:8888", "Address to config server")
+	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
+	flag.Parse()
+
+	fmt.Printf("Specified configBranch: %s\n", configBranch)
+
+	viper.Set("profile", *profile)
+	viper.Set("config_server_url", *configServerURL)
+	viper.Set("config_branch", *configBranch)
+}
+
 func main() {
 	fmt.Printf("Starting %v\n", appName)
+
+	config.LoadConfigurationFromBranch(
+		viper.GetString("config_server_url"),
+		appName,
+		viper.GetString("profile"),
+		viper.GetString("config_branch"))
+
 	initializeBoltClient()
-	service.StartWebServer("8080")
+
+	go config.StartListener(appName, viper.GetString("amqp_server_url"), viper.GetString("config_event_bus"))
+
+	service.StartWebServer(viper.GetString("server_port"))
 }
 
 func initializeBoltClient() {
