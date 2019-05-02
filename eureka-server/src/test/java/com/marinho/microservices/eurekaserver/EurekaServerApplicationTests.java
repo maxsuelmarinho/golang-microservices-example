cab8531f75ec9312b69982df9fbfe34f2a08d737
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-05-02 00:19:00 -0300

    add service discovery with eureka server

---
+++ eureka-server/src/test/java/com/marinho/microservices/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.marinho.microservices.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+}
